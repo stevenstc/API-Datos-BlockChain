3b207e6cddcac6c44a000ff1ff68c6fe53d9fa0e
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2021-05-03 20:28:17 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -61,7 +61,7 @@ app.get('/api/ver/consumo/:cuenta',async(req,res) => {
 				"lectura-numero": numero,
 				"KW_registrados": varconsu[1],
 				"Tiempo_de_registro": varconsu[2],
-				"hora_humana": moment(varconsu[2]).format('MMMM Do YYYY, h:mm:ss a');
+				"hora_humana": moment(varconsu[2]).format('MMMM Do YYYY, h:mm:ss a')
 			}
 		}
     //console.log("https://shasta.tronscan.org/#/transaction/"+regconsu);
